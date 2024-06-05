4748bcdcc42908a1fadb5359253e5f556397bc2f
YIzhilaiFanbo <a2753941060@gmail.com>
2024-06-06 00:15:19 +0800

    web0008_吴润

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using dms.Models;
+using System.Linq;
 
 namespace dms.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly DmsContext _context;
+        private const int PageSize = 10; // 每页显示的通知数
+
+        public StudentController(DmsContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Notifications(int pageIndex = 0)
+        {
+            var totalNotifications = _context.Notifications.Count();
+            var notifications = _context.Notifications
+                .OrderByDescending(n => n.CreatedAt)
+                .Skip(pageIndex * PageSize)
+                .Take(PageSize)
+                .ToList();
+
+            ViewBag.HasPreviousPage = pageIndex > 0;
+            ViewBag.HasNextPage = (pageIndex + 1) * PageSize < totalNotifications;
+            ViewBag.PageIndex = pageIndex;
+
+            return View(notifications);
         }
     }
 }
